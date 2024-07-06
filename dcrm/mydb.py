057c3ae2e363a6174c34ee248223f3eefc08c5f9
Natasha  Tatenda Chirombe <chirombenatashatatenda@gmail.com>
2024-07-06 16:59:16 +0300

    feat:add login and logout users

--- dcrm/mydb.py
+++ dcrm/mydb.py
@@ -3,7 +3,7 @@ import mysql.connector
 dataBase = mysql.connector.connect(
      host="localhost",
      user="root",
-     passwd="roo",
+     passwd="root",
 
 )
 
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
+
+def home(request):
+    # first check if user is logged in
+    if request.method == 'POST':
+       username = request.POST['username']
+       password = request.POST['password']
+
+       user = authenticate(request, username=username, password=password)     
+
+       if user is not None:
+           login(request, user)
+           messages.success(request, "You are logged in!")
+           return redirect('home')
+       else:
+           messages.success(request, "There was an error logging in!")
+           return redirect('home')
+    else:       
+       return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
